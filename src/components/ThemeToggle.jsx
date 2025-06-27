87b91dc0dd5b450824962732aa1d4ac86d8ec0d2
Muhammad Mujtaba <mujtabausman21@gmail.com>
2025-06-28 01:36:51 +0500

    Theme toggle updated

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { cn } from "@/lib/utils";
 
 export const ThemeToggle = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(true);
 
   useEffect(() => {
     const storedTheme = localStorage.getItem("theme");
@@ -11,15 +11,15 @@ export const ThemeToggle = () => {
       setIsDarkMode(true);
       document.documentElement.classList.add("dark");
     } else {
-      localStorage.setItem("theme", "light");
-      setIsDarkMode(false);
+      localStorage.setItem("theme", "dark");
+      setIsDarkMode(true);
     }
   }, []);
 
   const toggleTheme = () => {
     if (isDarkMode) {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
+      // document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "dark");
       setIsDarkMode(false);
     } else {
       document.documentElement.classList.add("dark");
@@ -36,11 +36,11 @@ export const ThemeToggle = () => {
         "focus:outlin-hidden"
       )}
     >
-      {isDarkMode ? (
+      {/* {isDarkMode ? (
         <Sun className="h-6 w-6 text-primary" />
       ) : (
         <Moon className="h-6 w-6 text-blue-900" />
-      )}
+      )} */}
     </button>
   );
 };
