fd3a5fb29957a0a1a6f6c5b049ab2cf8abb7a488
Muhammad Mujtaba <mujtabausman21@gmail.com>
2025-06-27 20:14:02 +0500

    Theme toggle updated

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -32,12 +32,12 @@ export const ThemeToggle = () => {
     <button
       onClick={toggleTheme}
       className={cn(
-        "fixed top-8 lg:top-5 right-5 z-50 sm:p-8 p-1 rounded-full transition-colors duration-300",
+        "fixed top-8 lg:top-5 right-5 z-50 p-1 rounded-full transition-colors duration-300",
         "focus:outlin-hidden"
       )}
     >
       {isDarkMode ? (
-        <Sun className="h-6 w-6 text-blue-600" />
+        <Sun className="h-6 w-6 text-primary" />
       ) : (
         <Moon className="h-6 w-6 text-blue-900" />
       )}
